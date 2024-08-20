3f4ae01235364dbee9172e3399c4aff0c36f9ce7
SorenCharlie <sorenr2209@gmail.com>
2024-08-19 23:04:35 -0700

    updated html and api routes so buttons work and notes show on left side of page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router(); //gets router level middleware
 const apiRoutes = require('./apiRoutes');
+const hmtlRoutes = require('./htmlRoutes');
 
 // routes to apiRoutes
 router.use('/api', apiRoutes);
+router.use('/', hmtlRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -15,17 +15,5 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.static("public"));
 app.use(routes);
 
-// not needed per express day 1 video
-// // Route to return the notes.html file
-// app.get('/notes', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'notes.html'));
-//   });
-  
-//   // Route to return the index.html file
-//   app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-//   });
-  
- 
 
 app.listen(PORT, () => console.log("Server is running"));
